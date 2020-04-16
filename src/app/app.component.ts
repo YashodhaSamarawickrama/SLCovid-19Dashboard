545041d8ca1cf05edbceb636ef5d0f5de0617274
Yashodha Samarawickrama <yasho.samarawickrama@gmail.com>
2020-04-17 00:20:57 +0530

    changed app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 
 
 @Component({
@@ -6,6 +6,12 @@ import { Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
 }
+
+
+
